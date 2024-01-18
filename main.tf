999491029689717bc8ec009ba7d3efd961933275
Liam Labuschagne <83798278+liamlabuschagne@users.noreply.github.com>
2024-01-18 18:30:12 +1300

    Add autoscaling

--- main.tf
+++ main.tf
@@ -29,20 +29,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name              = "blog-alb"
@@ -52,13 +55,10 @@ module "alb" {
 
   target_groups = {
     ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id = aws_instance.blog.id
-      backend_port     = 80
-      backend_protocol = "HTTP"
+      name_prefix       = "blog-"
+      target_type       = "instance"
+      backend_port      = 80
+      backend_protocol  = "HTTP"
     }
   }
 
